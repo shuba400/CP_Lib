ac69053447ab3e6bde39fe048a50ff3ad39b9644
Shubhang Singh <51315650+shuba400@users.noreply.github.com>
2022-02-18 12:25:06 +0530

    Create combinatorics.h

---
+++ Algorithm/combinatorics.h
@@ -0,0 +1,45 @@
+//Use this line right before debugger template statement for __print(mint t) to function
+template<int MOD>
+struct ModInt {
+    long long v;
+    ModInt(long long _v = 0) {v = (-MOD < _v && _v < MOD) ? _v : _v % MOD; if (v < 0) v += MOD;}
+    ModInt& operator += (const ModInt &other) {v += other.v; if (v >= MOD) v -= MOD; return *this;}
+    ModInt& operator -= (const ModInt &other) {v -= other.v; if (v < 0) v += MOD; return *this;}
+    ModInt& operator *= (const ModInt &other) {v = v * other.v % MOD; return *this;}
+    ModInt& operator /= (const ModInt &other) {return *this *= inverse(other);}
+    bool operator == (const ModInt &other) const {return v == other.v;}
+    bool operator != (const ModInt &other) const {return v != other.v;}
+    friend ModInt operator + (ModInt a, const ModInt &b) {return a += b;}
+    friend ModInt operator - (ModInt a, const ModInt &b) {return a -= b;}
+    friend ModInt operator * (ModInt a, const ModInt &b) {return a *= b;}
+    friend ModInt operator / (ModInt a, const ModInt &b) {return a /= b;}
+    friend ModInt operator - (const ModInt &a) {return 0 - a;}
+    friend ModInt power(ModInt a, long long b) {ModInt ret(1); while (b > 0) {if (b & 1) ret *= a; a *= a; b >>= 1;} return ret;}
+    friend ModInt inverse(ModInt a) {return power(a, MOD - 2);}
+    friend istream& operator >> (istream &is, ModInt &m) {is >> m.v; m.v = (-MOD < m.v && m.v < MOD) ? m.v : m.v % MOD; if (m.v < 0) m.v += MOD; return is;}
+    friend ostream& operator << (ostream &os, const ModInt &m) {return os << m.v;}
+};
+using mint = ModInt<mod>;
+
+
+
+
+//now paste this line somewhere after the modint
+const int N = 2e5 + 1;
+mint fact[N],inv_fact[N];
+
+
+void calc(ll n){
+    fact[0] = 1;
+    inv_fact[0] = 1;
+    rep(i,1,n){
+        fact[i] = fact[i - 1]*i;
+        inv_fact[i] = inv_fact[i - 1]/i;
+    }
+
+}
+
+mint nCr(ll n, ll r){
+    mint ans = fact[n]*inv_fact[r]*inv_fact[n - r];
+    return ans;
+}
